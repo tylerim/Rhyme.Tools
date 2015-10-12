15e20053581f569bdd8df0efcf1bcc962dbbd7e6
dylan.jang@7even.com <dylan.jang@7even.com>
2015-10-12 16:26:27 +0900

    - add socket multi connect / close tester.

--- Rhyme.Tools/Services/Socket/frmSocket.Designer.cs
+++ Rhyme.Tools/Services/Socket/frmSocket.Designer.cs
@@ -34,13 +34,24 @@
 			this.txtAddress = new System.Windows.Forms.TextBox();
 			this.txtPort = new System.Windows.Forms.TextBox();
 			this.btnClose = new System.Windows.Forms.Button();
+			this.gbTest1 = new System.Windows.Forms.GroupBox();
+			this.gbTest2 = new System.Windows.Forms.GroupBox();
+			this.txtMultiThreadCount = new System.Windows.Forms.TextBox();
+			this.label1 = new System.Windows.Forms.Label();
+			this.btnStartMulti = new System.Windows.Forms.Button();
+			this.btnStopMulti = new System.Windows.Forms.Button();
+			this.txtMultiIp = new System.Windows.Forms.TextBox();
+			this.txtMultiPort = new System.Windows.Forms.TextBox();
+			this.label2 = new System.Windows.Forms.Label();
+			this.gbTest1.SuspendLayout();
+			this.gbTest2.SuspendLayout();
 			this.SuspendLayout();
 			// 
 			// btnConnect
 			// 
-			this.btnConnect.Location = new System.Drawing.Point(158, 10);
+			this.btnConnect.Location = new System.Drawing.Point(131, 17);
 			this.btnConnect.Name = "btnConnect";
-			this.btnConnect.Size = new System.Drawing.Size(75, 23);
+			this.btnConnect.Size = new System.Drawing.Size(64, 25);
 			this.btnConnect.TabIndex = 0;
 			this.btnConnect.Text = "Connect";
 			this.btnConnect.UseVisualStyleBackColor = true;
@@ -48,9 +59,9 @@
 			// 
 			// btnSend
 			// 
-			this.btnSend.Location = new System.Drawing.Point(118, 37);
+			this.btnSend.Location = new System.Drawing.Point(97, 46);
 			this.btnSend.Name = "btnSend";
-			this.btnSend.Size = new System.Drawing.Size(75, 23);
+			this.btnSend.Size = new System.Drawing.Size(64, 25);
 			this.btnSend.TabIndex = 0;
 			this.btnSend.Text = "Send";
 			this.btnSend.UseVisualStyleBackColor = true;
@@ -58,53 +69,145 @@
 			// 
 			// txtSend
 			// 
-			this.txtSend.Location = new System.Drawing.Point(12, 39);
+			this.txtSend.Location = new System.Drawing.Point(6, 48);
 			this.txtSend.Name = "txtSend";
-			this.txtSend.Size = new System.Drawing.Size(100, 21);
+			this.txtSend.Size = new System.Drawing.Size(86, 20);
 			this.txtSend.TabIndex = 1;
 			this.txtSend.Text = "test";
 			// 
 			// txtAddress
 			// 
-			this.txtAddress.Location = new System.Drawing.Point(12, 12);
+			this.txtAddress.Location = new System.Drawing.Point(6, 19);
 			this.txtAddress.Name = "txtAddress";
-			this.txtAddress.Size = new System.Drawing.Size(100, 21);
+			this.txtAddress.Size = new System.Drawing.Size(86, 20);
 			this.txtAddress.TabIndex = 2;
 			this.txtAddress.Text = "192.168.0.40";
 			// 
 			// txtPort
 			// 
-			this.txtPort.Location = new System.Drawing.Point(118, 12);
+			this.txtPort.Location = new System.Drawing.Point(97, 19);
 			this.txtPort.Name = "txtPort";
-			this.txtPort.Size = new System.Drawing.Size(34, 21);
+			this.txtPort.Size = new System.Drawing.Size(30, 20);
 			this.txtPort.TabIndex = 3;
 			this.txtPort.Text = "6125";
 			// 
 			// btnClose
 			// 
-			this.btnClose.Location = new System.Drawing.Point(199, 39);
+			this.btnClose.Location = new System.Drawing.Point(167, 48);
 			this.btnClose.Name = "btnClose";
-			this.btnClose.Size = new System.Drawing.Size(75, 23);
+			this.btnClose.Size = new System.Drawing.Size(64, 25);
 			this.btnClose.TabIndex = 4;
 			this.btnClose.Text = "Close";
 			this.btnClose.UseVisualStyleBackColor = true;
 			this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
 			// 
+			// gbTest1
+			// 
+			this.gbTest1.Controls.Add(this.txtAddress);
+			this.gbTest1.Controls.Add(this.btnConnect);
+			this.gbTest1.Controls.Add(this.btnClose);
+			this.gbTest1.Controls.Add(this.btnSend);
+			this.gbTest1.Controls.Add(this.txtPort);
+			this.gbTest1.Controls.Add(this.txtSend);
+			this.gbTest1.Location = new System.Drawing.Point(12, 12);
+			this.gbTest1.Name = "gbTest1";
+			this.gbTest1.Size = new System.Drawing.Size(242, 85);
+			this.gbTest1.TabIndex = 5;
+			this.gbTest1.TabStop = false;
+			this.gbTest1.Text = "Connect test";
+			// 
+			// gbTest2
+			// 
+			this.gbTest2.Controls.Add(this.label2);
+			this.gbTest2.Controls.Add(this.txtMultiPort);
+			this.gbTest2.Controls.Add(this.txtMultiIp);
+			this.gbTest2.Controls.Add(this.btnStopMulti);
+			this.gbTest2.Controls.Add(this.btnStartMulti);
+			this.gbTest2.Controls.Add(this.label1);
+			this.gbTest2.Controls.Add(this.txtMultiThreadCount);
+			this.gbTest2.Location = new System.Drawing.Point(12, 103);
+			this.gbTest2.Name = "gbTest2";
+			this.gbTest2.Size = new System.Drawing.Size(335, 85);
+			this.gbTest2.TabIndex = 6;
+			this.gbTest2.TabStop = false;
+			this.gbTest2.Text = "Connect / close test";
+			// 
+			// txtMultiThreadCount
+			// 
+			this.txtMultiThreadCount.Location = new System.Drawing.Point(106, 19);
+			this.txtMultiThreadCount.Name = "txtMultiThreadCount";
+			this.txtMultiThreadCount.Size = new System.Drawing.Size(136, 20);
+			this.txtMultiThreadCount.TabIndex = 0;
+			this.txtMultiThreadCount.Text = "10";
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(23, 22);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(77, 13);
+			this.label1.TabIndex = 1;
+			this.label1.Text = "Thread count :";
+			// 
+			// btnStartMulti
+			// 
+			this.btnStartMulti.Location = new System.Drawing.Point(248, 19);
+			this.btnStartMulti.Name = "btnStartMulti";
+			this.btnStartMulti.Size = new System.Drawing.Size(75, 23);
+			this.btnStartMulti.TabIndex = 2;
+			this.btnStartMulti.Text = "Start";
+			this.btnStartMulti.UseVisualStyleBackColor = true;
+			this.btnStartMulti.Click += new System.EventHandler(this.btnStartMulti_Click);
+			// 
+			// btnStopMulti
+			// 
+			this.btnStopMulti.Location = new System.Drawing.Point(248, 48);
+			this.btnStopMulti.Name = "btnStopMulti";
+			this.btnStopMulti.Size = new System.Drawing.Size(75, 23);
+			this.btnStopMulti.TabIndex = 3;
+			this.btnStopMulti.Text = "Stop";
+			this.btnStopMulti.UseVisualStyleBackColor = true;
+			this.btnStopMulti.Click += new System.EventHandler(this.btnStopMulti_Click);
+			// 
+			// txtMultiIp
+			// 
+			this.txtMultiIp.Location = new System.Drawing.Point(106, 45);
+			this.txtMultiIp.Name = "txtMultiIp";
+			this.txtMultiIp.Size = new System.Drawing.Size(89, 20);
+			this.txtMultiIp.TabIndex = 4;
+			this.txtMultiIp.Text = "127.0.0.1";
+			// 
+			// txtMultiPort
+			// 
+			this.txtMultiPort.Location = new System.Drawing.Point(201, 45);
+			this.txtMultiPort.Name = "txtMultiPort";
+			this.txtMultiPort.Size = new System.Drawing.Size(41, 20);
+			this.txtMultiPort.TabIndex = 5;
+			this.txtMultiPort.Text = "1843";
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(37, 48);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(55, 13);
+			this.label2.TabIndex = 6;
+			this.label2.Text = "Endpoint :";
+			// 
 			// frmSocket
 			// 
-			this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(284, 262);
-			this.Controls.Add(this.btnClose);
-			this.Controls.Add(this.txtPort);
-			this.Controls.Add(this.txtAddress);
-			this.Controls.Add(this.txtSend);
-			this.Controls.Add(this.btnSend);
-			this.Controls.Add(this.btnConnect);
+			this.ClientSize = new System.Drawing.Size(368, 204);
+			this.Controls.Add(this.gbTest2);
+			this.Controls.Add(this.gbTest1);
 			this.Name = "frmSocket";
-			this.Text = "frmSocket";
+			this.Text = "Socket Tester";
+			this.gbTest1.ResumeLayout(false);
+			this.gbTest1.PerformLayout();
+			this.gbTest2.ResumeLayout(false);
+			this.gbTest2.PerformLayout();
 			this.ResumeLayout(false);
-			this.PerformLayout();
 
 		}
 
@@ -116,5 +219,14 @@
 		private System.Windows.Forms.TextBox txtAddress;
 		private System.Windows.Forms.TextBox txtPort;
 		private System.Windows.Forms.Button btnClose;
+		private System.Windows.Forms.GroupBox gbTest1;
+		private System.Windows.Forms.GroupBox gbTest2;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.TextBox txtMultiPort;
+		private System.Windows.Forms.TextBox txtMultiIp;
+		private System.Windows.Forms.Button btnStopMulti;
+		private System.Windows.Forms.Button btnStartMulti;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.TextBox txtMultiThreadCount;
 	}
 }
--- Rhyme.Tools/Services/Socket/frmSocket.cs
+++ Rhyme.Tools/Services/Socket/frmSocket.cs
@@ -20,6 +20,9 @@ namespace RhymeLoginTool.Z_Socket
 		}
 
 		private Socket _socket;
+		private List<Socket> _socketList = new List<Socket>();
+		private bool _isMultiStart = false;
+
 		private void btnConnect_Click(object sender, EventArgs e)
 		{
 			_socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
@@ -41,6 +44,59 @@ namespace RhymeLoginTool.Z_Socket
 				return;
 
 			_socket.Close();
+		}
+
+		private void btnStartMulti_Click(object sender, EventArgs e)
+		{
+			btnStopMulti_Click(this, EventArgs.Empty);
+
+			_isMultiStart = true;
+			var ipEndPoint = new IPEndPoint(IPAddress.Parse(txtMultiIp.Text), int.Parse(txtMultiPort.Text));
+			var threadCount = int.Parse(txtMultiThreadCount.Text);
+
+			for (var i = 0; i < threadCount; i++)
+			{
+				Task.Factory.StartNew(() =>
+				{
+					while (_isMultiStart)
+					{
+						var socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+						try
+						{
+							_socketList.Add(socket);
+
+							socket.Connect(ipEndPoint);
+							socket.Close();
+						}
+						catch (Exception)
+						{
+						}
+						finally
+						{
+							_socketList.Remove(socket);
+						}
+					}
+				});
+			}
+		}
+
+		private void btnStopMulti_Click(object sender, EventArgs e)
+		{
+			_isMultiStart = false;
+
+			foreach (var socket in _socketList.ToList())
+			{
+				try
+				{
+					if (socket != null)
+						socket.Close();
+				}
+				catch (Exception)
+				{
+				}
+			}
+
+			_socketList.Clear();
 		}
 	}
 }
